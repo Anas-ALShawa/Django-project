39155a174fff869a4b8092f8dcd95c4d7846abc2
anas <anas-shawa2025@hotmail.com>
2025-06-22 18:30:41 +0200

    Fix csrf

--- myfinal/settings.py
+++ myfinal/settings.py
@@ -20,7 +20,10 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-project-production-2203.up.railway.app',  # Replace with your actual Railway URL
+    'https://*.up.railway.app',  # This covers all Railway domains
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
